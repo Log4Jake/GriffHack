4f32eda7c7e6bbdf5a4f8f16a21a06fdf3848325
Log4Jake <62357760+Log4Jake@users.noreply.github.com>
2023-06-22 10:21:36 -0400

    Update griff-hack.py

--- griff-hack.py
+++ griff-hack.py
@@ -7,7 +7,7 @@ from scratchattach import Encoding
 from time import sleep
 username = 'YourUsername' #add your username The username field is case sensitive
 sessionId = "sessionID" #add your sessionID
-session = scratch3.Session(sessionId, username)
+session = scratch3.Session(sessionId, username=username)
 conn = session.connect_cloud('Your Remix project ID') #add your remixed project ID
 variables = scratch3.get_cloud('Your Remix project ID') #add your remixed project ID 
 conn = session.connect_cloud('843162693') #add Original Project ID
@@ -16,11 +16,11 @@ def run_forever():
         try:
                 while True:
                         for key in variables:
-                                conn = scratch3.TwCloudConnection(project_id = "Your Remix project ID", username) #add your remixed project ID 
+                                conn = scratch3.TwCloudConnection(project_id = "Your Remix project ID", username=username) #add your remixed project ID 
                                 var = conn.get_var(key)
                                 conn = session.connect_cloud('843162693') #add Original Project ID
                                 if var == 'None':
-                                        #do nothinig
+                                        #do nothing
                                         print(var)
                                 else:
                                         conn.set_var(key, var)
